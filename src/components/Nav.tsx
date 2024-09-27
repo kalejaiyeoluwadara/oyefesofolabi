8dfdb488eed663fcbc038c2db069793bb1202205
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-09-27 22:55:23 +0100

    minor updates

---
+++ src/components/Nav.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav className="sm:flex flex-col sm-gap-[44px] gap-[24px]">
+      <section>
+        <p>The Greatest</p>
+      </section>
+      <section></section>
+      <section></section>
+    </nav>
+  );
+}
+
+export default Nav;
