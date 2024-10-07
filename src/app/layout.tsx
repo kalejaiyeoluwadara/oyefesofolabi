67cf0678cadec080a5a78e016169dd29a70f7e5c
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-10-07 19:59:20 +0100

    favicon fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>{/* Favicon will be added automatically */}</head>
       <body>{children}</body>
     </html>
   );
